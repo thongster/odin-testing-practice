7591f20bd1a54db163858cc9f15d0a47f2127cb9
thongster <106802301+thongster@users.noreply.github.com>
2025-11-14 14:29:46 +0700

    reverse a string function and multiple tests

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-
 function capitalize(string) {
-    if (typeof string != "string" || 
-        string.length === 0 ||
-        typeof string[0] != "string"
-    ) return string
+  if (
+    typeof string != 'string' ||
+    string.length === 0 ||
+    typeof string[0] != 'string'
+  )
+    return string;
 
-    const newString = string[0].toUpperCase() + string.slice(1)
-    return newString
+  const newString = string[0].toUpperCase() + string.slice(1);
+  return newString;
 }
 
 function reverseString(string) {
+  let reversedStringArray = [];
+  for (const char of string.split('')) {
+    reversedStringArray.unshift(char);
+  }
+  const reversedString = reversedStringArray.join('');
+  return reversedString;
+}
 
-};
-
-export {capitalize, reverseString}
+export { capitalize, reverseString };
--- main.test.js
+++ main.test.js
@@ -1,27 +1,43 @@
-import {capitalize} from "./main.js"
+import { capitalize, reverseString } from './main.js';
 
 // tests for capitalize
 test('Capitalize first letter of string', () => {
-  expect(capitalize("try this here")).toBe("Try this here");
+  expect(capitalize('try this here')).toBe('Try this here');
 });
 
 test('Capitalize first letter of string', () => {
-  expect(capitalize("Try this here")).toBe("Try this here");
+  expect(capitalize('Try this here')).toBe('Try this here');
 });
 
 test('Capitalize first letter of string', () => {
-  expect(capitalize("a")).toBe("A");
+  expect(capitalize('a')).toBe('A');
 });
 
 test('Capitalize first letter of string', () => {
-  expect(capitalize("")).toBe("");
+  expect(capitalize('')).toBe('');
 });
 
 test('Capitalize first letter of string', () => {
-  expect(capitalize("_test")).toBe("_test");
+  expect(capitalize('_test')).toBe('_test');
 });
 
 test('Capitalize first letter of string', () => {
-  expect(capitalize("123")).toBe("123");
+  expect(capitalize('123')).toBe('123');
 });
 
+// tests for reverse a string
+test('Reverse a string', () => {
+  expect(reverseString('apple')).toBe('elppa');
+});
+
+test('Reverse a string', () => {
+  expect(reverseString('123456789')).toBe('987654321');
+});
+
+test('Reverse a string', () => {
+  expect(reverseString('_im in here123 !!')).toBe('!! 321ereh ni mi_');
+});
+
+test('Reverse a string', () => {
+  expect(reverseString('')).toBe('');
+});
